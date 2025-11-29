1da5e7b00f60ab193158ad6a743947ca702d548b
imaneAchour <imaneachur14@gmail.com>
2025-11-29 15:05:34 +0100

    Prepare for Railway deployment

--- elearning/urls.py
+++ elearning/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('courses/', include('courses.urls')),
     path('users/', include('users.urls', namespace='users')),
-    path('', RedirectView.as_view(url='/users/login/', permanent=False)),
+    path('', RedirectView.as_view(url='/users/login/', permanent=False)),  # redirige / vers login
 ]
